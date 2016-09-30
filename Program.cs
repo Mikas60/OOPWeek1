00b4953b55a2e1f662d3e8ec0fb6057456dd7a54
Mikas60 <Mikas60man@gmail.com>
2016-09-30 10:33:02 +0100

    Initialize
    Initialize with Hello World

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            string username = "Fabio Nunes";
+            Console.WriteLine("Hello " + username +"!");
+        }
+    }
+}
